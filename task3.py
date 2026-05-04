49e3fd33f97530a897e6b5b7012becbb3161d174
Sharad Pokharel <shrdpokharel@gmail.com>
2026-05-04 16:32:04 +0545

    Task3: Flatten nested list

---
+++ task3.py
@@ -0,0 +1,10 @@
+def flatten(my_list):
+	final_list=[]
+	for x in my_list:
+		if type(x)==list:
+			final_list.extend(flatten(x))
+		else:
+			final_list.append(x)
+	return final_list
+
+print(flatten([1, 2, [3, 4], 5, [6, 7, 8], 9]))
